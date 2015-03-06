6558281786ba138d2e2aa8490641da896b49a147
kalmanb <kalman@movio.co>
2015-03-06 23:32:12 +1300

    Added jade-mode, not working tho

---
+++ kb-jade/packages.el
@@ -0,0 +1,36 @@
+;;; packages.el --- kb-jade Layer packages File for Spacemacs
+;;
+;; Copyright (c) 2012-2014 Sylvain Benner
+;; Copyright (c) 2014-2015 Sylvain Benner & Contributors
+;;
+;; Author: Sylvain Benner <sylvain.benner@gmail.com>
+;; URL: https://github.com/syl20bnr/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+(defvar kb-jade-packages
+  '(
+    jade-mode
+    )
+  "List of all packages to install and/or initialize. Built-in packages
+which require an initialization must be listed explicitly in the list.")
+
+(defvar kb-jade-excluded-packages '()
+  "List of packages to exclude.")
+
+;; For each package, define a function kb-jade/init-<package-kb-jade>
+;;
+;; (defun kb-jade/init-my-package ()
+;;   "Initialize my package"
+;;   )
+;;
+;; Often the body of an initialize function uses `use-package'
+;; For more info on `use-package', see readme:
+;; https://github.com/jwiegley/use-package
+
+;; (defun kb-jade/init-jade-mode ()
+  ;; (use-package jade-mode
+    ;; :defer t))
+    ;; :mode "\\.jade\\'"))
