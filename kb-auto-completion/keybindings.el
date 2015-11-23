86e3ac7317dfc37003b6a1ad8d7fdf745e0d3aec
kalmanb <kalman@movio.co>
2015-11-23 22:03:47 +1300

    Add ggtags keybindings

---
+++ kb-auto-completion/keybindings.el
@@ -0,0 +1,8 @@
+
+;; Find def or reference
+(evil-leader/set-key "oo" 'ggtags-find-tag-dwim)
+(evil-leader/set-key "on" 'next-error)
+(evil-leader/set-key "op" 'previous-error)
+
+;; Update
+(evil-leader/set-key "ou" 'ggtags-update-tags)
