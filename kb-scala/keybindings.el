6041ff9219624bcfad39ea05f9708d6d83931aeb
kalmanb <kalman@movio.co>
2016-03-30 20:49:34 +1300

    Add more scala keybindings

--- kb-scala/keybindings.el
+++ kb-scala/keybindings.el
@@ -1,5 +1,7 @@
 
 ;; Scala quickfix
 (evil-global-set-key 'normal (kbd ";ff") 'scala-errors-goto-first-error)
+(evil-global-set-key 'normal (kbd ";fn") 'scala-errors-goto-next-error)
+(evil-global-set-key 'normal (kbd ";fp") 'scala-errors-goto-prev-error)
 
 (evil-global-set-key 'normal (kbd ";knf") 'new-file)
